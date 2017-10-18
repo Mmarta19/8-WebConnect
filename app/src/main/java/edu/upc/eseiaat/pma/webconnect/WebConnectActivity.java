ea5e5e22479c67f477c090f4c224b6d96ab43f52
Marta <martabarrachina1@hotmail.com>
2017-10-18 10:38:01 +0200

    Hem creat una nova classe, que va copiant els bytes d'entrada

--- app/src/main/java/edu/upc/eseiaat/pma/webconnect/WebConnectActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/webconnect/WebConnectActivity.java
@@ -2,6 +2,7 @@ package edu.upc.eseiaat.pma.webconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WebConnectActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class WebConnectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_connect);
+    }
+
+    public void connect(View view) {
+
     }
 }
